63624a0c88d0807f55d4e517543825ad5569f98b
shreypatel13 <90705974+shreypatel13@users.noreply.github.com>
2025-11-03 16:02:17 -0500

    Removing non-public locations
    One of the etiologies are confirmed norovirus that did not occur in an "Unknown" or "Private home/residence" setting

--- data_cleaning.R
+++ data_cleaning.R
@@ -1,6 +1,7 @@
 library(tidyverse)
 library(zoo)
 
+settings_to_omit = c("Unknown", "Private home/residence", NA)
 nors = read.csv("NORS.csv", header=TRUE) %>%
   select(-tail(names(.), 5)) %>%
   mutate(Date = as.yearmon(paste(.$Year, .$Month), "%Y %m")) %>%
@@ -11,6 +12,9 @@ nors = read.csv("NORS.csv", header=TRUE) %>%
   separate_wider_delim(Etiology.Status, delim=";", too_few="align_start", too_many="drop",
                        names=c("Status1", "Status2", "Status3", "Status4",
                                "Status5", "Status6")) %>%
+  separate_wider_delim(Setting, delim=";", too_few="align_start", too_many="drop",
+                       names=c("Setting1", "Setting2", "Setting3", "Setting4",
+                               "Setting5", "Setting6")) %>%
   mutate(Noro1 = ifelse(grepl("Norovirus", .$Etiology1), 1, 0),
          Noro2 = ifelse(grepl("Norovirus", .$Etiology2), 1, 0),
          Noro3 = ifelse(grepl("Norovirus", .$Etiology3), 1, 0),
@@ -22,10 +26,17 @@ nors = read.csv("NORS.csv", header=TRUE) %>%
          Confirmed3 = ifelse(grepl("Confirmed", .$Status3), 1, 0),
          Confirmed4 = ifelse(grepl("Confirmed", .$Status4), 1, 0),
          Confirmed5 = ifelse(grepl("Confirmed", .$Status5), 1, 0),
-         Confirmed6 = ifelse(grepl("Confirmed", .$Status6), 1, 0)) %>%
-  filter(Noro1*Confirmed1==1 | Noro2*Confirmed2==1 | Noro3*Confirmed3==1 |
-           Noro4*Confirmed4==1 | Noro5*Confirmed5==1 | Noro6*Confirmed6==1) %>%
-  select(-tail(names(.), 12))
+         Confirmed6 = ifelse(grepl("Confirmed", .$Status6), 1, 0),
+         Set1 = ifelse(.$Setting1 %in% settings_to_omit, 0, 1),
+         Set2 = ifelse(.$Setting2 %in% settings_to_omit, 0, 1),
+         Set3 = ifelse(.$Setting3 %in% settings_to_omit, 0, 1),
+         Set4 = ifelse(.$Setting4 %in% settings_to_omit, 0, 1),
+         Set5 = ifelse(.$Setting5 %in% settings_to_omit, 0, 1),
+         Set6 = ifelse(.$Setting6 %in% settings_to_omit, 0, 1)) %>%
+  filter(Noro1*Confirmed1*Set1==1 | Noro2*Confirmed2*Set2==1 |
+           Noro3*Confirmed3*Set3==1 | Noro4*Confirmed4*Set4==1 |
+           Noro5*Confirmed5*Set5==1 | Noro6*Confirmed6*Set6==1) %>%
+  select(-tail(names(.), 18))
 
 write.csv(nors, "NORS_cleaned.csv", row.names=FALSE)
 
