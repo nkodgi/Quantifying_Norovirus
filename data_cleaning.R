9c8a3c0f696054f853ee3676a7dbde843fda08fd
shreypatel13 <90705974+shreypatel13@users.noreply.github.com>
2025-11-03 15:43:45 -0500

    Reduced to confirmed Norovirus etiologies

--- data_cleaning.R
+++ data_cleaning.R
@@ -11,10 +11,25 @@ nors = read.csv("NORS.csv", header=TRUE) %>%
   separate_wider_delim(Etiology.Status, delim=";", too_few="align_start", too_many="drop",
                        names=c("Status1", "Status2", "Status3", "Status4",
                                "Status5", "Status6")) %>%
-  filter(if_any(c(Etiology1, Etiology2, Etiology3, Etiology4, Etiology5, Etiology6), 
-                ~ grepl("Norovirus", ., fixed=TRUE))) %>%
-  filter(if_any(c(Status1, Status2, Status3, Status4, Status5, Status5), 
-                ~ .=="Confirmed"))
+  # filter(if_any(c(Etiology1, Etiology2, Etiology3, Etiology4, Etiology5, Etiology6), 
+  #               ~ grepl("Norovirus", ., fixed=TRUE))) %>%
+  # filter(if_any(c(Status1, Status2, Status3, Status4, Status5, Status5), 
+  #               ~ .=="Confirmed")) %>%
+  mutate(Noro1 = ifelse(grepl("Norovirus", .$Etiology1), 1, 0),
+         Noro2 = ifelse(grepl("Norovirus", .$Etiology2), 1, 0),
+         Noro3 = ifelse(grepl("Norovirus", .$Etiology3), 1, 0),
+         Noro4 = ifelse(grepl("Norovirus", .$Etiology4), 1, 0),
+         Noro5 = ifelse(grepl("Norovirus", .$Etiology5), 1, 0),
+         Noro6 = ifelse(grepl("Norovirus", .$Etiology6), 1, 0),
+         Confirmed1 = ifelse(grepl("Confirmed", .$Status1), 1, 0),
+         Confirmed2 = ifelse(grepl("Confirmed", .$Status2), 1, 0),
+         Confirmed3 = ifelse(grepl("Confirmed", .$Status3), 1, 0),
+         Confirmed4 = ifelse(grepl("Confirmed", .$Status4), 1, 0),
+         Confirmed5 = ifelse(grepl("Confirmed", .$Status5), 1, 0),
+         Confirmed6 = ifelse(grepl("Confirmed", .$Status6), 1, 0)) %>%
+  filter(Noro1*Confirmed1==1 | Noro2*Confirmed2==1 | Noro3*Confirmed3==1 |
+           Noro4*Confirmed4==1 | Noro5*Confirmed5==1 | Noro6*Confirmed6==1) %>%
+  select(-tail(names(.), 12))
 
 # nors$Date = as.Date(paste(nors$Year, nors$Month, "01", sep = "-"))
 
